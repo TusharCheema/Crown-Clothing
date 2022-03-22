3ee55e7e10a8791172a8e9dc81800231d9570947
TusharCheema <tusheema@gmail.com>
2022-03-22 13:27:28 +0530

    updating directory page with directory and cateogry component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import HomePage from './homePage.component';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
